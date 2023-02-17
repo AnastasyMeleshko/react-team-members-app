db4c5b8cde7e91495ca761171d4985d413004bc2
AnastasyMeleshko <7634907@mail.ru>
2023-02-17 22:42:26 +0300

    feat: add initial markup for employees section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Header from './components/Header';
+import Footer from "./components/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js.map';
+import Employees from "./components/Employees";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Employees/>
+      <Footer/>
     </div>
   );
 }
