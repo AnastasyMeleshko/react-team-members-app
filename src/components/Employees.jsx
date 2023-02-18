84312077631df6555d972e5572ac6b98fa13db2d
AnastasyMeleshko <7634907@mail.ru>
2023-02-18 19:53:18 +0300

    feat: add logic for selected team members

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,105 +1,35 @@
-import React, {useState} from "react";
+import React from "react";
 import femaleProfile from "../images/femaleProfile.jpg";
 import maleProfile from "../images/maleProfile.jpg";
 
 
-const Employees = () => {
-
-    const [employees, setEmployees] = useState([
-        {
-            id: 1,
-            fullName: "Bob Jones",
-            designation: "JavaScript Developer",
-            gender: "male",
-            teamName: "TeamA"
-        },
-            {
-                id: 2,
-                fullName: "Jill Bailey",
-                designation: "Node Developer",
-                gender: "female",
-                teamName: "TeamA"
-            },
-            {
-                id: 3,
-                fullName: "Gail Shepherd",
-                designation: "Java Developer",
-                gender: "female",
-                teamName: "TeamA"
-            },
-            {
-                id: 4,
-                fullName: "Sam Reynolds",
-                designation: "React Developer",
-                gender: "male",
-                teamName: "TeamB"
-            },
-            {
-                id: 5,
-                fullName: "David Henry",
-                designation: "DotNet Developer",
-                gender: "male",
-                teamName: "TeamB"
-            },
-            {
-                id: 6,
-                fullName: "Sarah Blake",
-                designation: "SQL Server DBA",
-                gender: "female",
-                teamName: "TeamB"
-            },
-            {
-                id: 7,
-                fullName: "James Bennet",
-                designation: "Angular Developer",
-                gender: "male",
-                teamName: "TeamC"
-            },
-            {
-                id: 8,
-                fullName: "Jessica Faye",
-                designation: "API Developer",
-                gender: "female",
-                teamName: "TeamC"
-            },
-            {
-                id: 9,
-                fullName: "Lita Stone",
-                designation: "C++ Developer",
-                gender: "female",
-                teamName: "TeamC"
-            },
-            {
-                id: 10,
-                fullName: "Daniel Young",
-                designation: "Python Developer",
-                gender: "male",
-                teamName: "TeamD"
-            },
-            {
-                id: 11,
-                fullName: "Adrian Jacobs",
-                designation: "Vue Developer",
-                gender: "male",
-                teamName: "TeamD"
-            },
-            {
-                id: 12,
-                fullName: "Devin Monroe",
-                designation: "Graphic Designer",
-                gender: "male",
-                teamName: "TeamD"
-            }
-    ]);
+const Employees = ({employees, handleEmployeeCardClick, handleTeamSelectedChange, selectedTeam}) => {
 
     return(
         <main className="container">
+            <div className="row justify-content-center mt-3 mb-3">
+                <div className="col-6">
+                    <select name="teams" id="teams" className="form-select form-select-lg"
+                            value={selectedTeam} onChange={handleTeamSelectedChange}>
+                        <option value="teamA">Team A</option>
+                        <option value="teamB">Team B</option>
+                        <option value="teamC">Team C</option>
+                        <option value="teamD">Team D</option>
+                    </select>
+                </div>
+            </div>
             <div className="row justify-content-center mt-3 mb-3">
                 <div className="col-8">
                     <div className="card-collection">
                         {
                             employees.map((employee) => (
-                                <div id={employee.id} className="card m-2" style={{ cursor:"pointer" }}>
+                                <div id={employee.id} key={employee.id}
+                                     className={(
+                                         employee.teamName === selectedTeam ? "card m-2 standout" : "card m-2"
+                                     )}
+                                     style={{ cursor:"pointer" }}
+                                     onClick={handleEmployeeCardClick}
+                                >
 
                                     {(employee.gender === "female") ? <img src={femaleProfile} alt="profile-img" className="card-img-top"/>
                                                                     :   <img src={maleProfile} alt="profile-img" className="card-img-top"/>}
